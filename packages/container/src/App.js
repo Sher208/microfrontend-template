d17fbb8b4791eca41da70b1bf775e5eb8a4a300b
Thilakraj Shetty <thilakrajshetty11@gmail.com>
2022-11-13 21:15:29 +0530

    workflow v12

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <React.Fragment>
-      <h1>Create New App - V11</h1>
+      <h1>Create New App - V12</h1>
       <hr />
       <MarketingApp />
     </React.Fragment>
