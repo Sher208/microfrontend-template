df147d46158fa8279d56d7384b7e7b7735cadf41
Thilakraj Shetty <thilakrajshetty11@gmail.com>
2022-11-13 19:08:18 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <React.Fragment>
+      <h1>Create App !</h1>
+      <hr />
+      <MarketingApp />
+    </React.Fragment>
+  );
+};
